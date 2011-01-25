d0de3fe3fe655f90f1b3eae7bd1321bb847ad4e1
Daniel Bergey <bergey@alum.mit.edu>
2011-01-25 11:36:08 -0500

    rh event summary; sort output order; refactor

--- process.py
+++ process.py
@@ -6,8 +6,10 @@ from os.path import exists, join, basename
 import numpy as np
 from glob import glob
 import re
-from graphs import *
 import shutil
+graphs = False
+if graphs:
+    from graphs import *
 
 descre = re.compile(r'z(?P<z>\d)h(?P<h>\d+)s(?P<s>\d+)rh(?P<rh>\d+)v(?P<v>\d)')
 
@@ -59,7 +61,7 @@ def parse_name(scenario):
   desc = scenario['Desc']
   match = descre.search(desc)
   if match:
-    return [match.group(k) for k in ['s', 'z', 'h', 'v', 'rh']] + [desc]
+    return [int(match.group(k)) for k in ['s', 'z', 'h', 'v', 'rh']] + [desc]
   else:
     raise RuntimeError('Cannot interpret scenario name {0}'.format(scenario))
 
@@ -74,24 +76,32 @@ def collect_specs(spec_path, data_path):
 # the field order in name determines sort order
   return [(parse_name(s), scenario_path(s)) for s in spec]
 
-def summarize_csv(specs, out_csv):
+def output_row(desc, f, hourly, out_csv, first_run=False):
+    h, sum_vals = f(**hourly)
+    if first_run: 
+        # first row of output
+        out_csv.writerow( parse_name(None) + h )
+    out_csv.writerow(desc + sum_vals)
+
+def summarize_csv(specs):
+  first_run = True
+  general_csv = output_handle('summary')
+  rh_csv = output_handle('rh-thresholds')
   for desc, scenario_path in specs:
     name = desc[-1]
     if exists(scenario_path):
         print "loading {0} from {1}".format(name, scenario_path)
         hourly = hourly_data(scenario_path)
         print "summarizing {0}".format(name)
-        h, sum_vals = summarize_run(**hourly)
-        print "graphing {0}".format(name)
-        plot_TRH(name, hourly)
-        plot_Wrt(name, hourly)
-        plot_rh_hist(name, hourly)
-        plot_t_hist(name, hourly)
-        if not head: 
-          # first row of output
-            head = parse_name(None) + h # save head from first scenario
-            out_csv.writerow(head)
-        out_csv.writerow(desc + sum_vals)
+        output_row(desc, summarize_run, hourly, general_csv, first_run)
+        output_row(desc, rh_stats, hourly, rh_csv, first_run)
+        first_run = False # flag, never becomes true again in this call
+        if graphs:
+            print "graphing {0}".format(name)
+            plot_TRH(name, hourly)
+            plot_Wrt(name, hourly)
+            plot_rh_hist(name, hourly)
+            plot_t_hist(name, hourly)
     else:
 # scenario path does not exist
         print "skipping {0}: path {1} does not exist".format(name, scenario_path)
@@ -128,28 +138,51 @@ def long_events(condition, length):
       count += 1
   return count
 
-# take the simulation outputs and summarize
-# return a pair: headings in order, value list in matching order
-def summarize_run(RHi, Ti, C_i, Qsac, Qlac, ACKW, RTFc, RTFe, RTFh, RTFrh, RTFacf, RTFd, RTFdf, rtfvf, rtfxf, rtfhf, **hourly):
-    heads = []
-    vals = []
+def rh_above_threshold(threshold, RHi):
+    i = np.where(RHi > threshold, 1, 0)
+    heads = ['hours above {0}% RH'.format(threshold)]
+    vals = [i.sum()]
 
-    # Overall RH Data
-    i = np.where(RHi > 60, 1, 0)
-    heads.append('mean RH')
-    vals.append(RHi.mean())
-
-    heads.append('hours above 60% RH')
-    vals.append(i.sum())
-
-    heads.append('max RH')
-    vals.append(RHi.max())
-    
 # RH events over 4, 8 hours
     heads.append('RH events 60% for 4+ hours')
     vals.append( long_events(RHi > 60, 4) )
     heads.append('RH events 60% for 8+ hours')
     vals.append( long_events(RHi > 60, 8) )
+
+    return (heads, vals)
+
+def rh_stats(RHi, **hourly):
+    heads = ['mean RH']
+    vals = [RHi.mean()]
+
+    heads.append('max RH')
+    vals.append(RHi.max())
+
+    for threshold in [50, 55, 60, 65]:
+      h, v = rh_above_threshold(threshold, RHi)
+      heads.extend(h)
+      vals.extend(v)
+
+    return (heads, vals)
+    
+
+# take the simulation outputs and summarize
+# return a pair: headings in order, value list in matching order
+def summarize_run(RHi, Ti, C_i, Qsac, Qlac, ACKW, RTFc, RTFe, RTFh, RTFrh, RTFacf, RTFd, RTFdf, rtfvf, rtfxf, rtfhf, KWHT, FANKW, DKW, DFANKW, KWVF, KWXF, KWHF, **hourly):
+    heads = []
+    vals = []
+
+    # Overall RH Data
+    heads.append('mean RH')
+    vals.append(RHi.mean())
+
+    heads.append('max RH')
+    vals.append(RHi.max())
+
+    h, v = rh_above_threshold(60, RHi)
+    heads.extend(h)
+    vals.extend(v)
+
 
     heads.append('average T')
     vals.append(Ti.mean())
@@ -179,20 +212,16 @@ def summarize_run(RHi, Ti, C_i, Qsac, Qlac, ACKW, RTFc, RTFe, RTFh, RTFrh, RTFac
       vals.append( ac_btu_yr / ac_kwh_yr / 1e3 )
     
     # Various Runtimes
-    runtimes = ['RTFc', 'RTFe', 'RTFh', 'RTFrh', 'RTFacf', 'RTFd', 'RTFdf', 'rtfvf',
-            'rtfxf', 'rtfhf']
-    for v in runtimes: 
-      if not v in locals():
-            print "Missing {0}".format(v)
-           # exec v + ' = asarray([0.])'
 
     vals += [
-        RTFc.sum(),     # AC Runtime
         RTFc.max(),     # Max hourly fraction
+        RTFh.max(),     # Max hourly fraction
+        RTFc.sum(),     # AC Runtime
         RTFe.sum(),     # Econ Runtime
         RTFh.sum(),     # Heating Runtime
         RTFrh.sum(),    # ReHeat Runtime
         RTFacf.sum(),   # Supply Fan Runtime
+        RTFacf.sum() - RTFc.sum() - RTFh.sum(), # AHU for vent & mixing
         RTFd.sum(),     # Dehumid Runtime
         RTFdf.sum(),    # Des Fan Runtime
         rtfvf.sum(),    # Vent Damper / Fan Runtime
@@ -201,34 +230,50 @@ def summarize_run(RHi, Ti, C_i, Qsac, Qlac, ACKW, RTFc, RTFe, RTFh, RTFrh, RTFac
         ]
 
     heads += [
-      'AC Runtime',
       'Max Cooling in One Hour',
+      'Max Heating in One Hour',
+      'AC Runtime',
       'Econ Runtime',
       'Heating Runtime',
       'ReHeat Runtime',
       'AHU Fan Runtime',
+      'AHU Runtime for vent & mixing',
       'Dehumid Runtime',
       'Des Fan Runtime',
       'Vent Damper / Fan Runtime',
       'Exhaust Fan Runtime',
       'HRV Runtime' ]
 
-# Total energy consumption
-    annual_kWh = [
-        (ACKW * RTFc).sum(),                 # AC Electric Use (kWh)
-        (KWHT * RTFh).sum(),                 # Heater Electric Use (kWh)
-        (RTFacf * FANKW).sum(),              # Supply Fan Electric Use (kWh)
-        (RTFd * DKW).sum(),                  # Des Unit Electric Use (kWh)
-        (RTFdf * DFANKW).sum(),              # Des FAN Electric Use (kWh)
-        (rtfvf * KWVF).sum(),                # Vent Damp/Fan Electric Use (kWh)
-        (rtfxf * KWXF).sum(),                # Exhaust Fan Electric Use (kWh)
-        (rtfhf * KWHF).sum(),                # HRV Electric Use (kWh)
-    ]
-    heads.append('Annual kWh (HVAC)')
-    vals.append( annual_kWh.sum() )
+# Don't bother reporting Qrh; RTFrh should be 0
+    annual_kWh = {
+      'cooling': (ACKW * RTFc).sum(),      # AC Electric Use (kWh)
+      'heating': (KWHT * RTFh).sum(),      # Heater Electric Use (kWh)
+      'AHU': (RTFacf * FANKW).sum(),       # Supply Fan Electric Use (kWh)
+      'dehumidifier': (RTFd * DKW).sum(),  # Des Unit Electric Use (kWh)
+      'DH fan': (RTFdf * DFANKW).sum(),    # Des FAN Electric Use (kWh)
+      'vent damper': (rtfvf * KWVF).sum(), # Vent Damp/Fan Electric Use (kWh)
+      'exhaust fan': (rtfxf * KWXF).sum(),     # Exhaust Fan Electric Use (kWh)
+      'HRV': (rtfhf * KWHF).sum(),         # HRV Electric Use (kWh)
+    }
 
-    #fmt_str = ','.join(['%.1f' for each in l]) + ','
-    #f.write(fmt_str % tuple(l))
+# submetered energy consumption
+    for key, value in annual_kWh.iteritems():
+      heads.append('annual {0} kWh'.format(key))
+      vals.append(value)
+
+    heads.append('AHU Fan energy for Cooling')
+    vals.append( (RTFc * FANKW).sum() )
+
+    heads.append('AHU Fan energy for Heating')
+    vals.append( (RTFh * FANKW).sum() )
+
+    heads.append('AHU fan energy for Ventilation and Mixing')
+    vals.append( ( (RTFacf - RTFc - RTFh) * FANKW ).sum() )
+
+
+# Total energy consumption
+    heads.append('Total annual kWh')
+    vals.append(sum(annual_kWh.values()))
 
     return (heads, vals)
 
@@ -238,18 +283,25 @@ def concat(lst):
     ret += l
   return ret
 
-def output_handle( ):
-  output_path = dt.datetime.now().strftime('%Y-%m-%d-%H:%M-summary.csv')
+def output_handle(name):
+  output_path = dt.datetime.now().strftime('%Y-%m-%d-%H%M-{0}.csv'.format(name))
   file = open(output_path, 'wb')
   return csv.writer(file)
 
 if __name__ == '__main__':
-  print sys.argv
   if len(sys.argv) >= 3 and exists(sys.argv[1]) and exists(sys.argv[2]):
     data_path = sys.argv[1]
-    out_file = output_handle()
-    specs = concat( [ collect_specs(c) for c in sys.argv[2:] ] )
+    specs = concat( [ collect_specs(c, data_path) for c in sys.argv[2:] ] )
     specs.sort()
-    summarize_csv( specs, out_file )
+    summarize_csv( specs )
   else:
-    print """No usage summary yet; read the code"""
+    print """{0} DATAPATH SPEC...
+    look in DATAPATH for data files (for_##.dat)
+    look in SPEC for descriptions of each run
+
+    {0} expects the data files to be in arranged hierarchically.
+    It expects one directory in DATAPATH for each SPEC listed on the command
+    line, having the same name as SPEC without the .csv extension.
+    Within each directory, there should be one folder named Run# for each line
+    in SPEC.  Each Run# directory should have all for_##.dat files for that run.
+    """
