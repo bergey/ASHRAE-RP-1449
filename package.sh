588f3f3cba3bc81320d36db743b266a67472ffe5
Daniel Bergey <bergey@alum.mit.edu>
2011-01-28 11:31:53 -0500

    undo rsync

--- package.sh
+++ package.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/sh
-rsync -v 1449.TRD package
-rsync -v TRN_Resdh4.py package
-rsync -v buildings/* package
-rsync -v lookups/* package
-rsync -v res_dh_sched.dat package
-rsync -vr package /mnt/flash/package
+cp 1449.TRD package
+cp TRN_Resdh4.py package
+cp buildings/* package
+cp lookups/* package
+cp res_dh_sched.dat package
+cp -r sim-specs package
+cp -r package/ /mnt/flash
