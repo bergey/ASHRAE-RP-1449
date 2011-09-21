20eaca6a0e5b07d732414f718d7cb7c3816d2123
Daniel Bergey <bergey@alum.mit.edu>
2011-09-21 12:40:14 -0400

    update package script to match types dir rename

--- package.sh
+++ package.sh
@@ -3,7 +3,7 @@ cp 1449.TRD package
 cp TRN_Resdh4.py package
 cp buildings/* package
 cp lookups/* package
-cp types/* package
+cp types-compile/*dll package
 cp res_dh_sched.dat package
 rm -r package/sim-specs
 cp -r sim-specs package
