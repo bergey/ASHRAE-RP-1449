e1f52e4c82096c52ab78ca66222f9ccbcadf65be
Daniel Bergey <bergey@alum.mit.edu>
2011-12-19 15:02:35 -0500

    add many more psychrometric functions

--- postprocessing/physics.py
+++ postprocessing/physics.py
@@ -41,7 +41,6 @@ def vapour_P_ice(T):
 def psychrometer(tdb,twb,P=1.01325e5):
     """returns the relative humidity (wrt liquid water, between 0 and 1) given
     the dry bulb and wet-bulb temperature (in K)"""
-    P = 1.01325e5;
     Wsdash = 0.62198*vapour_P(twb)/(P-vapour_P(twb));
     W = ((2501 - 2.381*(twb-273.15))*Wsdash - 1.006*(tdb-twb))/\
         (2502+1.805*(tdb-273.15)-4.186*(twb-273.15));
@@ -68,3 +67,43 @@ def humidity_ratio(rh, T):
     xw = rh*xws
     w = 0.621945*xw*P/(P-xw*P)
     return w
+
+def rh(w, T):
+    """reversing humidity_ratio, above)"""
+    P = 101325
+    pws = vapour_PF(T)
+    xws = pws/P
+    xw = w/(0.621945+w)
+    rh = xw/xws
+    return rh
+
+def enthalpy(W, T):
+    """T in degF, return h in BTU/lb_da"""
+    return 0.240*T + W*(1061 + 0.444*T)
+
+def enthalpy_RH(RH, T):
+    return enthalpy(humidity_ratio(RH, T), T)
+
+def f2K(T):
+    return (T-32)/1.8+273
+
+def K2f(T):
+    return (T-273)*1.8+32
+
+def wet_bulb(RH, Tdb, low=-40, high=None):
+    if RH < 0 or RH > 1:
+        raise Exception("RH out of range: {0}".format(RH))
+    if high==None:
+        high = Tdb
+    def wet_bulb_(RH, Tdb, low, high, err=0.001):
+        while True:
+            wb = (low+high)/2
+            test = psychrometer(Tdb, wb)
+            if abs(RH-test) < err:
+                return wb
+            else:
+                if test < RH:
+                    low = wb
+                else:
+                    high = wb
+    return K2f(wet_bulb_(RH, f2K(Tdb), f2K(low), f2K(high)))
