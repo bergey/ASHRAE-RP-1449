b271806305c224105cb77af366b931ef170c7459
Daniel Bergey <bergey@alum.mit.edu>
2012-01-05 17:49:22 -0500

    refactor gensim by variable, bug-compatible
    this version produces identical output to the previous but is
    organized with all assignments for a given TRD variable in one place,
    rather than all settings for a DH or vent system.  I hope that this
    will make it easier to understand the relationship between systems.
    It makes adding a neXw system marginally more tedious, but debugging
    easier.  It is already easy to see what is set in any single system,
    by inspecting csv or TRD.
    
    In rewriting this code, I uncovered a number of bugs in the old
    gensim, as well as several intended but unmade changes.  These should
    be in the following commits.

--- sim-specs/gensim.py
+++ sim-specs/gensim.py
@@ -13,7 +13,11 @@ SENS_BASE = 72700 # BTU/day
 LATG_BASE = 12 # lbs/day
 
 def order_line(d):
-  return [d['Desc'],None]+[d[h] for h in head[2:]]
+  try:
+    return [d['Desc'],None]+[d[h] for h in head[2:]]
+  except:
+    print(d)
+    raise
 
 bno_file = open('../buildings/Single_Zone_Buildings.txt')
 bno_lines = csv.reader(bno_file)
@@ -52,6 +56,20 @@ def ach_to_ela(ach):
   ela = sm * (100/2.54)**2
   return ela
 
+def hers_to_ach(h):
+  if h==50:
+    return 3
+  elif h==70:
+    return 4
+  elif h==85:
+    return 5
+  elif h==100:
+    return 7
+  elif h==130:
+    return 10
+  else:
+    raise
+
 # simulations are parameterized as follows:
 # z zone 1--5
 #
@@ -77,82 +95,29 @@ def ach_to_ela(ach):
 #   add heat pipe or dessicant unit
 def sim_line(z,h,s,rh,v):
 # exclude unimplemented scenarios
+  # no explicit rh control
+  # don't use ERV in cold climates
+  # or HRV in warm climates; z3 gets both
+  # unventilated HERS 130, except system 1 comparison
+#  if (rh==60 and s in [1, 3, 4]) or (v==3 and z>3) or (v==4 and z<3) or (h==130 and v!=0 and s!=1) or (v==0 and h!= 130 and s!=1)
+  if (s==1 and rh==60) or (v==1 and h==130 and s!=1) or (v==2 and h==130) or (v in (3,4) and h==130) or (v in (3,4) and s in (5,6,7,9)) or (v==4 and z>3) or (v==3 and z<3):
+     return None
+     
+
 
   Run = 1 # update in enclosing code
+
   BaseFile = '1449.TRD'
-  Desc = 'z{0}h{1}s{2}rh{3}v{4}'.format(z,h,s,rh,v) # TODO add more parameters
+
+  Desc = 'z{0}h{1}s{2}rh{3}v{4}'.format(z,h,s,rh,v) # TODO add more parameters for sensitivity
+  
   SinZone_bno = get_bno(z,h)
 
-  LATG_DAILY = LATG_BASE
-
-# parameters depending only on HERS
-  if h==50:
-    ELA = ach_to_ela(3)
-    ANO = 19 # for comparison; overridden by system TODO
-    # HSPF in post-processing
-    WCFM_H = 0.35
-    SENS_DAILY = SENS_BASE*0.7
-    sduct_area = 0
-    rduct_area = 0
-    leaks = 0
-    leakr = 0
-    duct_Rval = 1
-    Ht_QIN = 40000
-  elif h==70:
-    ELA = ach_to_ela(4)
-    ANO = 19 # for comparison; overridden by system TODO
-    # HSPF in post-processing
-    WCFM_H = 0.35
-    SENS_DAILY = SENS_BASE*0.8
-    if z==5:
-        sduct_area = 100
-    elif z==4:
-        sduct_area = 250
-    else:
-        sduct_area = 544
-    rduct_area = 100
-    leaks = 0.03
-    leakr = 0.02
-    duct_Rval = 8
-    Ht_QIN = 40000
-  elif h==85:
-    ELA = ach_to_ela(5)
-    ANO = 18 # Overridden by system 3, 4
-    WCFM_H = 0.35
-    SENS_DAILY = SENS_BASE*0.9
-    sduct_area = 544 # overridden by system 4
-    rduct_area = 100 # overridden by system 4
-    leaks = 0.03 # 5% total leakage, split 60/40
-    leakr = 0.02
-    duct_Rval = 8
-    Ht_QIN = 60000
-  elif h==100:
-    ELA = ach_to_ela(7)
-    ANO = 17
-    WCFM_H = 0.5
-    SENS_DAILY = SENS_BASE
-    sduct_area = 544
-    rduct_area = 100
-    leaks = 0.06 # 10% total
-    leakr = 0.04
-    duct_Rval = 6
-    Ht_QIN = 60000
-  elif h==130:
-    ELA = ach_to_ela(10)
-    ANO = 16
-    WCFM_H = 0.5
-    SENS_DAILY = SENS_BASE
-    sduct_area = 544
-    rduct_area = 100
-    leaks = 0.12
-    leakr = 0.08
-    duct_Rval = 6
-    Ht_QIN = 60000
-  else:
-    print("Shouldn't get here: HERS {0}".format(h))
-    return None
-
-# each vector is: Orlando, Miami, Houston, Atlanta, Nashville, Indianapolis
+  ELA = ach_to_ela(hers_to_ach(h))
+  
+  WeatherFile = ['Orlando-FL-3', 'Miami-FL-3', 'Houston-TX-3', 'Atlanta-GA-3', 'Nashville-TN-3', 'Indianapolis-IN-3'][z]
+  
+  # vector is: Orlando, Miami, Houston, Atlanta, Nashville, Indianapolis
   hp_tonnage = {130: [3,3,3,2.,2.5,2.5],
                 100: [3,3,3,2.5,2.5,2.5],
                  85: [2.5,2.5,2.5,2,2,2],
@@ -160,261 +125,225 @@ def sim_line(z,h,s,rh,v):
                  50: [2,2,2,2,2,2]}
   ACTON = hp_tonnage[h][z] # 0-indexed array, starting with Orlando
 
-# parameters depending on zone, or zone and HERS
-  if z==1:
-    WeatherFile = 'Miami-FL-3'
-    Ht_QIN = 40000
-    THhi = 72
-    THlo = 72
-  elif z==2:
-    WeatherFile = 'Houston-TX-3'
-    THhi = 72
-    THlo = 72
-  elif z==3:
-    WeatherFile = 'Atlanta-GA-3'
-    THhi = 70
-    THlo = 70
-  elif z==4:
-    WeatherFile = 'Nashville-TN-3'
-    THhi = 70
-    THlo = 70
-  elif z==5:
-    WeatherFile = 'Indianapolis-IN-3'
-    THhi = 70
-    THlo = 70
-  elif z==0:
-    WeatherFile = 'Orlando-FL-3'
-    THhi = 72
-    THlo = 72
-
-# defaults
   ACCFM = ACTON*375
-  HCFM = ACTON*275
-  Res_DNO = 21
-  DS_TYPE = 0
-  DSET = 45 # not used
-  DCFM_AHU = 148
-  DCFM_no_AHU = 148
-  REGEN = 0
-  DSIN_OPT = 1
-  DSIN_VAL = 0 # not used
-  RSCHD = 0
-  DSOUT = 1
 
-# parameters depending only on DH system
-  if s==1:
-    if rh == 60:
-      return None # no RH setpoint for system 1
-    if h<85:
-      ANO = 18 # only for calbiration, direct comparison to HERS 85 system 1; remove for final publication
-    #ACTON = 2
-        # turn off standalone dehumidifier
-    HUM_CNTL_type = 0  # No enhanced DH
-    ilck61 = 0 # don't run DH
-    Humlo_0 = 99 # actually not critical, because TRD does this internally
-    Humhi_0 = 99
-  else: # all systems except 1, 3, 4
-    Humlo_0 = rh
-    Humhi_0 = rh
+  # TODO system 12
+  if s==3:
+    ANO = 19
+  elif s==4:
+    ANO = 20
+  elif s==1 and h<85:
+    ANO = 18
+  elif s==12:
+    ANO = {130: 21,
+           100: 22,
+            85: 23,
+            70: 24,
+            50: 24}[h]
+  else:
+    ANO = {130: 16,
+           100: 17,
+            85: 18,
+            70: 19,
+            50: 19}[h]
+
+  if h<=70 or z==1: # TODO add z==0
+    Ht_QIN = 40000
+  else:
+    Ht_QIN = 60000
+  
+  HCFM = ACTON*275
+
+  if z<= 2:
+    THhi = 72
+    THlo = 72
+  else:
+    THhi = 70
+    THlo = 70
 
   if s==2:
     HUM_CNTL_type = 3
-    ilck61 = 0 # don't run DH
-
-  if s==3 or s==4:
-    if s==3:
-        ANO = 19
-    elif s==4:
-        ANO = 20
-        sduct_area = 0 # override HERS-based ductwork
-        rduct_area = 0 # ductless minisplit
-        leaks = 0
-        leakr = 0
-        duct_Rval = 1
-    else:
-        raise UserException("Expected system 3 or 4")
-    HUM_CNTL_type = 0  # No enhanced DH
-    ilck61 = 0 # don't run DH
+  elif s==8:
+    HUM_CNTL_type = 5 # Lennox
+  elif s==10:
+    HUM_CNTL_type = 4
+  elif s==11:
+    HUM_CNTL_type = 2
+  else:
+    HUM_CNTL_type = 0 # no explicit DH Control by AC
+  
+  if s==5:
+    Res_DNO = 21
+  elif s==6:
+    Res_DNO = 21 # TODO get new dh line
+  elif s==7:
+    Res_DNO = 21 # TODO get line for s7
+  else:
+    Res_DNO = 21 # TODO change default to 1
+  
+  if s in (6,7):
+    DS_TYPE = 2
+  else:
+    DS_TYPE = 0 # always use size from lookup file
+  
+  if s==6:
+    DSET = 65
+  elif s==7:
+    DSET = 90
+  else:
+    DSET = 45 # TODO take this out
+  
+  if s==5:
+    DCFM_AHU = 148
+    DCFM_no_AHU = 148
+  elif s==6:
+    DCFM_AHU = 100
+    DCFM_no_AHU = 120
+  elif s==7:
+    DCFM_AHU = 183
+    DCFM_no_AHU = 220
+  else:
+    DCFM_AHU = 148 # TODO default to 0 after passing regr
+    DCFM_no_AHU = 148
+  
+  REGEN = 0 # TODO where does this change?
+  
+  if s==7:
+    DSIN_OPT = 2
+    DSIN_VAL = 0.67 # 2:1, mostly recirc
+  else:
+    DSIN_OPT = 1
+    DSIN_VAL = 0 # not used
+    
+  RSCHD = 0
+  
+  DSOUT = 1
+  
+  if s in [1, 3, 4]:
     Humlo_0 = 99
     Humhi_0 = 99
+  else:
+    Humlo_0 = rh
+    Humhi_0 = rh
+    
+  if h<=85:
+    WCFM_H = 0.35
+  else:
+    WCFM_H = 0.5
 
-  if s==5:
-      HUM_CNTL_type = 0 # No active dehumification by AC
-      Res_DNO = 21
-      DS_TYPE = 0 # DSET from lookup file
-      DCFM_AHU = 148
-      DCFM_no_AHU = 148
-      REGEN = 0 # reject heat to interior
-      DSIN_OPT = 1 # draw air from interior
-      DSIN_VAL = 0 # not used
-      RSCHD = 0 # recirc mode off
-      DSOUT = 1 # supply air sent to space (== supply duct)
-      ilck61 = 3 # run DH fan when DH is running
-      fctyp5 = 3 # cycle AHU 5 minutes out of 30
-      ftim_ON5 = recirc(ACTON)
-      ftim_OFF5 = 1 - ftim_ON5
-
-  if s==6:
-      HUM_CNTL_type = 0 # No active dehumification by AC
-      Res_DNO = 21 # TODO will be 22; for now we use system 5 DH
-      DS_TYPE = 2
-      DSET = 65
-      DCFM_AHU = 100
-      DCFM_no_AHU = 120
-      REGEN = 0 # reject heat to interior
-      DSIN_OPT = 1 # draw air from inside
-      DSIN_VAL = 0 # not used
-      RSCHD = 0 # recirc mode off
-      DSOUT = 1 # supply air sent to space (== supply duct)
-      ilck61 = 3 # run DH fan when DH is running
-      fctyp5 = 3 # cycle AHU for 0.5 ACH
-      ftim_ON5 = recirc(ACTON)
-      ftim_OFF5 = 1 - ftim_ON5
-
-  if s==7:
-      HUM_CNTL_type = 0 # No active dehumification by AC
-      Res_DNO = 21 # TODO will be 22; for now we use system 5 DH
-      DS_TYPE = 2 # DSET from lookup file
-      DSET = 90 # based on email <87ehy0z0y2.wl%daniel@buildingscience.com>
-      DCFM_AHU = 183 # scaling DCFM_no_AHU by 100/120, from system 6
-      DCFM_no_AHU = 220 # from Armin's email cited above
-      REGEN = 0 # reject heat to interior
-      DSIN_OPT = 2 # mix indoor and outdoor air
-      DSIN_VAL = 0.67 # 100 CFM recirc, 50 CFM OA
-      RSCHD = 0 # recirc mode off
-      DSOUT = 1 # supply air sent to space (== supply duct)
-      ilck61 = 3 # run DH fan when DH is running
-      fctyp5 = 3 # cycle AHU for 0.5 ACH
-      ftim_ON5 = recirc(ACTON)
-      ftim_OFF5 = 1 - ftim_ON5
-      fctyp7 = 5 # same as CFIS
-      ilck71 = 3 # open vent when DH is running
-      ftim_ON7 = vent0 / (DCFM_no_AHU * (1-DSIN_VAL)) # on time to achieve 62.2
-
-  if s==8:
-      HUM_CNTL_type = 5 # Lennox Dehumidification
-      ilck61 = 0 # don't run DH
-
-  if s==10:
-    HUM_CNTL_type = 4
-    ilck61 = 0 # don't run DH
-
-  if s==11:
-    HUM_CNTL_type = 2
-    ilck61 = 0 # don't run DH
-
-  if s==12:
-    HUM_CNTL_type = 0
-    ilck61 = 0 # don't run DH
-    # override AC to include heat pipe
-    # AC depends on HERS
-    if h==130:
-      ANO = 21
-    elif h==100:
-      ANO = 22
-    elif h==85:
-      ANO = 23
-    elif h==70:
-      ANO = 24
-    elif h==50:
-      ANO = 24
-
-# Ventilation systems
-  if v==0: # No ventilation
-    VCFM = 0
-    exh_cfm = 0
-    HRV_CFM = 0
-    HRV_W = 0
-    if not 'fctyp5' in locals():
-      # fan cycling not required by DH system
-      fctyp5 = 0
-      ftim_ON5 = 0
-      ftim_OFF5 = 0
-    fctyp7 = 0
-    ftim_ON7 = 0
-    ftim_OFF7 = 0
-    ilck71 = 0
-    fctyp8 = 0
-    fctyp9 = 0
-    ftim_ON9 = 0.0
-    ftim_OFF9 = 0.0
-    ilck91 = 0
+  if v==3:
+    HRV_eS = 0.75
+    HRV_eL = 0
+  elif v==4:
+    HRV_eS = 0.7
+    HRV_eL = 0.6
+  else:
     HRV_eS = 0
     HRV_eL = 0
-  elif v==1: # Exhaust only
-    if h == 130 and s != 1:
-      return None
+
+  if v==2:
+    VCFM = vent0*3
+  else:
     VCFM = 0
+
+  if v==1:
     exh_cfm = vent0
-    HRV_CFM = 0
-    HRV_W = 0
-    if not 'fctyp5' in locals():
-      # fan cycling not required by DH system
-      fctyp5 = 0
-      ftim_ON5 = 0
-      ftim_OFF5 = 0
-    fctyp7 = 0
-    ftim_ON7 = 0
-    ftim_OFF7 = 0
-    ilck71 = 0
-    fctyp8 = 1
-    fctyp9 = 0
-    ftim_ON9 = 0.0
-    ftim_OFF9 = 0.0
-    ilck91 = 0
-    HRV_eS = 0
-    HRV_eL = 0
-  elif v==2: # CFIS
-    if h == 130:
-      return None
-    VCFM = vent0*3 # average vent0 over the hour
+  else:
     exh_cfm = 0
-    HRV_CFM = 0
-    HRV_W = 0
-    fctyp5 = 3 # CFIS requires more fan runtime than DH mixing
-    ftim_ON5 = 0.17 # 33% of hour, two cycles per hour
-    ftim_OFF5 = 0.33
-    fctyp7 = 5
-    ftim_ON7 = 0.17
-    ftim_OFF7 = 0.33
-    ilck71 = 5
-    fctyp8 = 0
-    fctyp9 = 0
-    ftim_ON9 = 0.0
-    ftim_OFF9 = 0.0
-    ilck91 = 0
-    HRV_eS = 0
-    HRV_eL = 0
-  elif v==3 or v==4: # HRV or ERV
-    if h == 130 or s in [5,6,7,9]:
-      return None
-    if v==4: # ERV
-      HRV_eS = 0.7
-      HRV_eL = 0.6
-      if z>3: # Don't use ERV in cold climates
-          return None
-    else: # HRV
-      HRV_eS = 0.75
-      HRV_eL = 0
-      if z<3: # prefer ERV; in zone 3 either HRV or ERV
-          return None
-    # parameters the same for ERV or HRV
-    VCFM = 0
-    exh_cfm = 0
+  
+  if v==3 or v==4:
     HRV_CFM = 2*vent0 # twice 62.2 for 50% of hour
     HRV_W = 0.5*HRV_CFM # 0.5 W/CFM per Task 4 report
+  else:
+    HRV_CFM = 0
+    HRV_W = 0
+    
+  if v==2:
+    fctyp5 = 3  # AHU fan cycling
+    ftim_ON5 = 0.17 # 33% of hour, two cycles per hour
+    ftim_OFF5 = 0.33
+  elif v==3 or v==4:
     fctyp5 = 3
-    ftim_ON5 = 0.5 # concurrent with HRV
+    ftim_ON5 = 0.5  # concurrent with HRV
     ftim_OFF5 = 0.5
+  elif s in [5, 6, 7]:
+    fctyp5 = 3
+    ftim_ON5 = recirc(ACTON)
+    ftim_OFF5 = 1 - ftim_ON5
+  else:
+    fctyp5 = 0
+    ftim_ON5 = 0
+    ftim_OFF5 = 0
+    
+  if v==2:
+    fctyp7 = 5
+    ilck71 = 5
+    ftim_ON7 = 0.17
+    ftim_OFF7 = 0.33
+# TODO add this back in when passes regression
+  # elif s==7: # never when v==2
+  #   fctyp7 = 5
+  #   ilck71 = 3
+  #   ftim_ON7 = vent0 / (DCFM_no_AHU * (1-DSIN_VAL)) # on time to achieve 62.2
+  #   ftim_OFF7 = 1 - ftim_ON7
+  else:
     fctyp7 = 0
+    ilck71 = 0
     ftim_ON7 = 0
     ftim_OFF7 = 0
-    ilck71 = 0
-    fctyp8 = 0
+    
+  if v==1:
+    fctyp8 = 1
+  else:
+    fctyp8 =  0
+
+  if v==3 or v==4:
     fctyp9 = 5
     ftim_ON9 = 0.5
     ftim_OFF9 = 0.5
     ilck91 = 5
+  else:
+    fctyp9 = 0
+    ftim_ON9 = 0.0
+    ftim_OFF9 = 0.0
+    ilck91 = 0
+
+    
+  if s==4 or h==50:
+    sduct_area = 0
+  elif h==70:
+    sduct_area = [544, 544, 544, 544, 250, 100][z]
+  else:
+    sduct_area = 544
+
+  if s==4 or h==50:
+    rduct_area = 0
+  else:
+    rduct_area = 100
+
+  if s==4:
+    leaks = 0
+    leakr = 0
+  else:
+    leaks, leakr = { 50: (0,0),
+                     70: (0.03, 0.02),
+                     85: (0.03, 0.02),
+                    100: (0.06, 0.04),
+                    130: (0.12, 0.08) }[h]
+  if s==4 or h==50:
+    duct_Rval = 1
+  elif h<=85:
+    duct_Rval = 8
+  else:
+    duct_Rval = 6
+
+  SENS_DAILY = SENS_BASE*{130: 1,
+                          100: 1,
+                           85: 0.9,
+                           70: 0.8,
+                           50: 0.7}[h]
+  
+  LATG_DAILY = LATG_BASE # TODO add sensitivity cases
 
 
 # put all of the variables used above into a list in a consistent order
@@ -579,7 +508,7 @@ def sys_12_13_14():
     print("{0} lines in {1}".format(lcount, filename))
 
 
-#by_system([1,2,3,4,5,6,7,8])
-#sys_10_11()
-#sys_12_13_14()
-short(1, 2, 3, 4, 5, 6,7,8,)
+by_system([1,2,3,4,5,6,7,8])
+sys_10_11()
+sys_12_13_14()
+#short(1, 2, 3, 4, 5, 6,7,8,)
